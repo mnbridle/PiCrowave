bec069b6ab385765c333f3de4564035a4cf8ae23
Matthew Bridle <mnbridle@gmail.com>
2022-10-06 23:10:43 +1300

    Bugfix

--- ui/spectral_plot.py
+++ ui/spectral_plot.py
@@ -17,11 +17,11 @@ def show_band(fb):
 
     for x in range(20, 300, 16):
         draw.line((x, 15, x, 225), fill=(255, 255, 255, 0))
-        draw.text((x, 0), 2400 + x, font=small_fnt, fill=(0, 255, 0, 0))
+        draw.text((x, 0), f"{2400 + x}", font=small_fnt, fill=(0, 255, 0, 0))
 
     for y in range(15, 225, 30):
         draw.line((0, y, 300, y), fill=(255, 255, 255, 0))
-        draw.text((0, y), -30 - y, font=small_fnt, fill=(0, 255, 0, 0))
+        draw.text((0, y), f"{-30 - y}", font=small_fnt, fill=(0, 255, 0, 0))
 
     # get a font
     fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 12)
