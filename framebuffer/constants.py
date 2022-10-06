9fde7f141a2a34ab0f54e829ff8e2b98f218f5f4
Matthew Bridle <mnbridle@gmail.com>
2022-10-06 21:34:17 +1300

    Add license; use pytorinox framebuffer code with PIL

--- framebuffer/constants.py
+++ framebuffer/constants.py
@@ -1,2 +1,3 @@
 screen_width = 320
 screen_height = 240
+framebuffer_number = 1
--- main.py
+++ main.py
@@ -1,21 +1,57 @@
+from framebuffer.framebuffer import Framebuffer
+from framebuffer import constants
+
 import time
-from framebuffer import framebuffer
+from PIL import ImageDraw, Image, ImageFont
+import matplotlib.pyplot as plt
+import random
+import io
+
+
+def TestFrameBuffer():
+    fb = framebuffer.Framebuffer(constants.framebuffer_number)
+
+    # Initialise image in framebuffer
+    image = Image.new("RGBA", fb.size)
+    draw = ImageDraw.Draw(image)
+    draw.rectangle(((0, 0), fb.size), fill="red")
+    draw.ellipse(((0, 0), fb.size), fill="blue", outline="red")
+
+    # get a font
+    fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 12)
+
+    # draw multiline text
+    draw.multiline_text((10, 10), "Hello\nWorld", font=fnt, fill=(0, 0, 0))
+
+    # Plot something using matplotlib
+
+    # # channel RSSI
+    # randomlist = []
+    # for i in range(0,13):
+    #     n = random.randint(-90,-20)
+    #     randomlist.append(n)
+
+    # print(randomlist)
+
+    # plt.rcParams["figure.figsize"] = [7.50, 3.50]
+    # plt.rcParams["figure.autolayout"] = True
+
+    # fig = plt.figure(figsize=(320/72, 240/72), dpi=72)
+    # ax = fig.add_axes([0, 0, 1, 1])
+    # channels = [chan for chan in range(1, 14)]
+
+    # ax.plot(channels, randomlist)
+
+    # img_buf = io.BytesIO()
+    # plt.savefig(img_buf, format='png')
+
+    # im = Image.open(img_buf)
+    # fb.show(im)
+
+    # img_buf.close()
+
 
 def main():
-    lcd_screen = framebuffer.FB_GFXInterface(320, 240)
-    
-    lcd_screen.init()
-    lcd_screen.write_screen()
-    
-    for idx, circle_radius in enumerate(range(8, 128, 8)):
-        lcd_screen.circle(160, 120, circle_radius, fg_shade=idx*2)
-
-    for y in range(16, 240, 16):
-        lcd_screen.horizontal_line(y=y, fg_shade=15)
-
-    for x in range(16, 320, 16):
-        lcd_screen.vertical_line(x=x, fg_shade=15)
-
-    lcd_screen.write_screen()
+    TestFrameBuffer()
 
 main()
