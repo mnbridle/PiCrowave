70c86a9dcdace8215a2c6adbd13405bde37fcf0f
Matthew Bridle <mnbridle@gmail.com>
2022-10-06 22:57:04 +1300

    Bugfix

--- ui/spectral_plot.py
+++ ui/spectral_plot.py
@@ -16,14 +16,14 @@ def show_band(fb):
     draw.rectangle(((0, 0), fb.size), fill="black")
 
     for x in range(0, 320, 18):
-        draw.line((x, 15), (x, 225), fill=(255,255,255,0))
+        draw.line((x, 15), (x, 225), fill=[255,255,255,0])
 
     for y in range(15, 240, 30):
-        draw.line((0, y), (320, y), fill=(255,255,255,0))
+        draw.line((0, y), (320, y), fill=[255,255,255,0])
 
     # get a font
     fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 12)
 
     # draw multiline text
-    draw.multiline_text((10, 10), "Hello\nWorld", font=fnt, fill=(0, 255, 0, 0))
+    draw.multiline_text((10, 10), "Hello\nWorld", font=fnt, fill=[0, 255, 0, 0])
     fb.show(image)
