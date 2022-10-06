83b3cd161d706fd8f5e088600c6e598eca1676e4
Matthew Bridle <mnbridle@gmail.com>
2022-10-06 23:03:26 +1300

    Bugfix

--- ui/spectral_plot.py
+++ ui/spectral_plot.py
@@ -14,10 +14,10 @@ def show_band(fb):
     # Every 10dBm -> 7 lines -> 240/7 = 30
 
     for x in range(0, 320, 18):
-        draw.line((x, 15, x, 225), fill=(255, 255, 255, 0), width=10)
+        draw.line((x, 15, x, 225), fill=(255, 255, 255, 0))
 
     for y in range(15, 240, 30):
-        draw.line((0, y, 320, y), fill=(255, 255, 255, 0), width=10)
+        draw.line((0, y, 320, y), fill=(255, 255, 255, 0))
 
     # get a font
     fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 12)
