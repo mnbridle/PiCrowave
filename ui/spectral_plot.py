2cf03eb7d8f1e744b436bb61d2791e055e5a0ef8
Matthew Bridle <mnbridle@gmail.com>
2022-10-06 23:09:36 +1300

    Text labels

--- ui/spectral_plot.py
+++ ui/spectral_plot.py
@@ -13,11 +13,15 @@ def show_band(fb):
     # RSSI from -96 to -26 = 70dBm
     # Every 10dBm -> 7 lines -> 240/7 = 30
 
-    for x in range(0, 320, 18):
-        draw.line((x, 15, x, 225), fill=(255, 255, 255, 0))
+    small_fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 6)
 
-    for y in range(15, 240, 30):
-        draw.line((0, y, 320, y), fill=(255, 255, 255, 0))
+    for x in range(20, 300, 16):
+        draw.line((x, 15, x, 225), fill=(255, 255, 255, 0))
+        draw.text((x, 0), 2400 + x, font=small_fnt, fill=(0, 255, 0, 0))
+
+    for y in range(15, 225, 30):
+        draw.line((0, y, 300, y), fill=(255, 255, 255, 0))
+        draw.text((0, y), -30 - y, font=small_fnt, fill=(0, 255, 0, 0))
 
     # get a font
     fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 12)
