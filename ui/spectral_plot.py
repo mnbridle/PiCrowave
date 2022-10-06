953686acf59938243e4cad32adb6e1ea922d8861
Matthew Bridle <mnbridle@gmail.com>
2022-10-06 23:11:45 +1300

    Fix text

--- ui/spectral_plot.py
+++ ui/spectral_plot.py
@@ -13,11 +13,11 @@ def show_band(fb):
     # RSSI from -96 to -26 = 70dBm
     # Every 10dBm -> 7 lines -> 240/7 = 30
 
-    small_fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 6)
+    small_fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 10)
 
     for x in range(20, 300, 16):
         draw.line((x, 15, x, 225), fill=(255, 255, 255, 0))
-        draw.text((x, 0), f"{2400 + x}", font=small_fnt, fill=(0, 255, 0, 0))
+        draw.text((x, 300), f"{2400 + x}", font=small_fnt, fill=(0, 255, 0, 0))
 
     for y in range(15, 225, 30):
         draw.line((0, y, 300, y), fill=(255, 255, 255, 0))
