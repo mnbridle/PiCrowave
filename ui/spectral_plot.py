608292e883c6d7c54ed424b32157f9897745a527
Matthew Bridle <mnbridle@gmail.com>
2022-10-10 21:43:27 +1300

    Change frequency labels to increment every 40MHz

--- ui/spectral_plot.py
+++ ui/spectral_plot.py
@@ -29,13 +29,13 @@ def show_band(fb):
 
     for x in range(40, 320, 20):
         draw.line((x, 20, x, 220), fill="green")
-        label = f"{2380 + (x/2)}"
-        draw.text((x, 220), label.rjust(6), font=small_fnt, fill="green")
+        if (y-20 % 40 == 0):
+            label = f"{2380 + (x/2)}"
+            draw.text((x, 220), label.rjust(6), font=small_fnt, fill="green")
 
     for y in range(20, 220, 20):
         draw.line((40, y, 320, y), fill="white")
-        if (y-20 % 40 == 0):
-            label = f"{-30 - (y-20)/2}"
-            draw.text((0, y+15), label.rjust(6), font=small_fnt, fill="green")
+        label = f"{-30 - (y-20)/2}"
+        draw.text((0, y+15), label.rjust(6), font=small_fnt, fill="green")
 
     fb.show(image)
