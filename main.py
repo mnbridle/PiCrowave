c44bdd5774f1936acdc5e846b6ad768dc53639b9
Matthew Bridle <mnbridle@gmail.com>
2022-10-10 22:30:22 +1300

    Keep regenerating data

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def main():
     fb = Framebuffer(constants.framebuffer_number)
 
     # Get test data
-    rf_data = test_data.full_spectrum_fft(min_freq=2400000, max_freq=2540000, freq_resolution=5000)
-    ui.spectral_plot.show_band(fb, rf_data)
+    for i in range(0, 10):
+        rf_data = test_data.full_spectrum_fft(min_freq=2400000, max_freq=2540000, freq_resolution=5000)
+        ui.spectral_plot.show_band(fb, rf_data)
 
 main()
