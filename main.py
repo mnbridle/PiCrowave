37cbe3e0334e69b55de3038c9b7f62eae0d791e5
Matthew Bridle <mnbridle@gmail.com>
2022-10-10 22:16:57 +1300

    Extra args for test data

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     fb = Framebuffer(constants.framebuffer_number)
 
     # Get test data
-    rf_data = test_data.full_spectrum_fft()
+    rf_data = test_data.full_spectrum_fft(min_freq=2400000, max_freq=2540000)
     ui.spectral_plot.show_band(fb, rf_data)
 
 main()
--- wifi/test_data.py
+++ wifi/test_data.py
@@ -1,10 +1,10 @@
 import math
 import numpy as np
 
-def full_spectrum_fft():
+def full_spectrum_fft(min_freq=2400000, max_freq=2483500, min_power=-96, max_power=-30):
     # Generate full-spectrum FFT data, from 2400 to 2483.5MHz, data points every 312.5kHz
-    frequencies = np.arange(2400000, 2483500, 312.5)
-    rssi = np.random.randint(low=-96, high=-30, size=(frequencies.size))
+    frequencies = np.arange(min_freq, max_freq, 312.5)
+    rssi = np.random.randint(low=min_power, high=max_power, size=(frequencies.size))
 
     data = np.dstack((frequencies, rssi))
     data = np.squeeze(data)
