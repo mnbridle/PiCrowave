b02b534e69b8febe9bb25aa159f3306fd60e9748
Matthew Bridle <mnbridle@gmail.com>
2022-10-10 21:35:41 +1300

    Update spectral plot

--- ui/spectral_plot.py
+++ ui/spectral_plot.py
@@ -22,22 +22,17 @@ def show_band(fb):
     # Bottom right corner: (300, 220)
 
     shape = [(0, 0), (320, 240)]
-    draw.rectangle(shape, fill="#000000", outline="white")
+    draw.rectangle(shape, fill="#000000", outline="black")
 
     shape = [(20, 20), (300, 220)]
     draw.rectangle(shape, fill="#101010", outline="white")
 
-    for x in range(20, 300, 20):
+    for x in range(40, 320, 20):
         draw.line((x, 20, x, 220), fill="green")
         draw.text((x, 220), f"{2380 + (x/2)}", font=small_fnt, fill="green")
 
     for y in range(20, 220, 20):
-        draw.line((20, y, 300, y), fill=(255, 255, 255, 0))
+        draw.line((40, y, 320, y), fill="white")
         draw.text((0, y), f"{-30 - (y-20)/2}", font=small_fnt, fill="green")
 
-    # get a font
-    fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 12)
-
-    # draw multiline text
-    draw.multiline_text((10, 10), "Hello\nWorld", font=fnt, fill="green")
     fb.show(image)
