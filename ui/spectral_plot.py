44be15e3b81e5e2539506992a3f2b7217002a278
Matthew Bridle <mnbridle@gmail.com>
2022-10-10 22:09:32 +1300

    Make yellow points

--- ui/spectral_plot.py
+++ ui/spectral_plot.py
@@ -48,6 +48,6 @@ def show_band(fb, rf_data):
     for frequency in range(2400, 2540, 5):
         x = int(((frequency - 2400) * 2) + 40)
         y = int((-((rf_power + 20) * 2)) + 20)
-        draw.ellipse([(x-5, y-5), (x+5, y+5)], fill="yellow")
+        draw.ellipse([(x-1, y-1), (x+1, y+1)], fill="yellow")
 
     fb.show(image)
