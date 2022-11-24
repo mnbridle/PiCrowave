5e574b46960e5c02f95c051ce1cfbd997406176b
Matthew Bridle <mnbridle@gmail.com>
2022-11-24 22:22:18 +1300

    Timestamps

--- wifi/athspectralscan/athspectralscandecoder.py
+++ wifi/athspectralscan/athspectralscandecoder.py
@@ -130,6 +130,7 @@ class AthSpectralScanDecoder(object):
                         cuml_rf_data[:, 1] = np.maximum(rf_data[:, 1], cuml_rf_data[:, 1])
                 self.output_queue.put((ts, cuml_rf_data))
                 ts = []
+                samples = []
 
     @staticmethod
     def _decode(data, no_pwr=False):
