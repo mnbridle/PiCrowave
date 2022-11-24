ffca5719d5b0ec268544d0b560e9b5ab5872d1ca
Matthew Bridle <mnbridle@gmail.com>
2022-11-24 20:51:22 +1300

    Improved plotting

--- ui/spectral_plot.py
+++ ui/spectral_plot.py
@@ -3,7 +3,7 @@ from PIL import ImageDraw, Image, ImageFont
 import copy
 
 
-def initialise_image(fb, channel, location=(30, 20), size=(280, 200)):
+def initialise_image(fb, channel, location=(30, 20), size=(280, 200), freq_scale=(2400, 2483), pwr_scale=(-150, -30), gridlines=(1, 10)):
     # Initialise image in framebuffer
     image = Image.new("RGBA", fb.size, (0, 0, 0, 0))
     draw = ImageDraw.Draw(image)
@@ -18,45 +18,20 @@ def initialise_image(fb, channel, location=(30, 20), size=(280, 200)):
     shape = [location, tuple([sum(tup) for tup in zip(location, size)])]
     draw.rectangle(shape, fill="#101010", outline="white")
 
-    draw.text((0, 0), f"Spectrum analysis - channel {channel}", font=hdg_fnt, fill="yellow")
-
-    return {"image": image, "draw": draw, "small_fnt": small_fnt, "hdg_fnt": hdg_fnt}
-
-def show_channel(fb, channel_number: int = 1):
-    pass
-
-def show_band(fb, rf_data, image_data, location=(30, 20), size=(280, 200), gridlines=(1, 10), autoscale=False):
-    image = copy.copy(image_data["image"])
-    draw = copy.copy(image_data["draw"])
-    small_fnt = copy.copy(image_data["small_fnt"])
-    hdg_fnt = copy.copy(image_data["hdg_fnt"])
-
-    # Find min and max of each axes in the dataset
-    freq_min, pwr_min = rf_data.min(axis=0)
-    freq_max, pwr_max = rf_data.max(axis=0)
-
-    if not autoscale:
-        pwr_min = -150
-        pwr_max = -30
+    # Draw gridlines
+    pwr_min, pwr_max = pwr_scale
+    freq_min, freq_max = freq_scale
 
     # Freq will be on x, pwr on y
     # Work out scalings
     freq_span = (freq_max - freq_min)
     pwr_span = abs(pwr_max - pwr_min)
 
-    # Next, work out MHz and dBm per pixel
-    freq_per_px = freq_span / size[0]
-    pwr_per_px = pwr_span / size[1]
-
-    # print(f"Freq per px: {freq_per_px}, pwr per px: {pwr_per_px}")
-
     # Work out interval for gridlines
     grid_intervals = [
         (gridlines[0] / freq_span) * size[0],
         (gridlines[1] / pwr_span) * size[1]
     ]
-
-    # print(f"Grid intervals: {grid_intervals}")
 
     x = location[0]
     y = location[1]
@@ -77,8 +52,33 @@ def show_band(fb, rf_data, image_data, location=(30, 20), size=(280, 200), gridl
         dbm -= gridlines[1]
         y += int(grid_intervals[1])
 
+    draw.text((0, 0), f"Spectrum analysis - channel {channel}", font=hdg_fnt, fill="yellow")
+
+    return {"image": image, "draw": draw, "small_fnt": small_fnt, "hdg_fnt": hdg_fnt}
+
+def show_channel(fb, channel_number: int = 1):
+    pass
+
+def show_band(fb, rf_data, image_data, location=(30, 20), size=(280, 200), freq_scale=(2400, 2483), pwr_scale=(-150, -30), gridlines=(1, 10)):
+    image = copy.copy(image_data["image"])
+    draw = copy.copy(image_data["draw"])
+    small_fnt = copy.copy(image_data["small_fnt"])
+    hdg_fnt = copy.copy(image_data["hdg_fnt"])
+
+    # Draw gridlines
+    pwr_min, pwr_max = pwr_scale
+    freq_min, freq_max = freq_scale
+
+    # Freq will be on x, pwr on y
+    # Work out scalings
+    freq_span = (freq_max - freq_min)
+    pwr_span = abs(pwr_max - pwr_min)
+
+    # Next, work out MHz and dBm per pixel
+    freq_per_px = freq_span / size[0]
+    pwr_per_px = pwr_span / size[1]
+
     old_data = None
-    print(len(rf_data))
     for data_point in rf_data:
         frequency, rf_power = data_point
 
