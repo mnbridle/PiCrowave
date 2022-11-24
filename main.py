cb5a920bc6f6eb40514d50df0ba2b45e16d03789
Matthew Bridle <mnbridle@gmail.com>
2022-11-24 19:13:55 +1300

    Make framebuffer copies immutable

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import time
 import numpy as np
@@ -14,15 +15,28 @@ def spectral_demo(fb, spectral_data, channel):
     # This will need to be fast
     # Get the data, average the samples, pump out a plot
 
+    timetrack = time.time()
+    image_data = copy.copy(ui.spectral_plot.initialise_image(fb, channel=channel))
+    print(f"Took {time.time() - timetrack} to generate the image")
+
+    old_fb = copy.copy(fb)
+
     while(1):
+        timetrack = time.time()
         start_time = time.time()
         while time.time() - start_time < 0.25:
             time.sleep(0.05)
 
+        print(f"Waited for {time.time() - timetrack}")
+
+        timetrack = time.time()
         rf_data = spectral_data.get_queue_data()
-        channel = int(os.system("sudo iw wlan1 info | grep channel | cut -d' ' -f2"))
-        image_data = ui.spectral_plot.initialise_image(fb, channel=channel)
-        ui.spectral_plot.show_band(fb, rf_data, image_data)
+        print(f"Time to get RF data: {time.time() - timetrack}")
+
+        timetrack = time.time()
+        new_fb = copy.copy(old_fb)
+        ui.spectral_plot.show_band(new_fb, rf_data, image_data)
+        print(f"Took {time.time() - timetrack} to plot the image")
 
 def main():
     channel = 7
